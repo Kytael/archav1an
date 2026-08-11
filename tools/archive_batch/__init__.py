c4b716e8d99490a9b9e842d69e8e4287823edafc
Kytael <2531906+Kytael@users.noreply.github.com>
2026-08-11 04:14:28 -0700

    archive-batch: package skeleton and pytest harness

---
+++ tools/archive_batch/__init__.py
@@ -0,0 +1,8 @@
+"""Scheduler for the 2001-2007 dance archive batch run.
+
+See the design notes, which are not part of this tree
+"""
+
+ARCHIVE_ROOT = "/mnt/media/dance"
+ENCODED_SUBDIR = "encoded"
+SOURCE_HOST = "gpu1"
